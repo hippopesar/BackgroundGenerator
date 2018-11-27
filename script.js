791ecbd87dab5ff19eaacf9258ad5b2e919f67d1
hippopesar <35821384+hippopesar@users.noreply.github.com>
2018-11-27 15:20:43 -0800

    match color and show css on load

--- script.js
+++ script.js
@@ -3,17 +3,34 @@ var clr1 = document.getElementsByName("color1")[0];
 var clr2 = document.getElementsByName("color2")[0];
 var h3 = document.getElementsByTagName("h3")[0];
 
-clr1.addEventListener("input", ChangeColor);
-clr2.addEventListener("input", ChangeColor);
+SetBackground(clr1.value, clr2.value);
+h3.textContent = body.style.background + ";";
 
-function ChangeColor(){
-    body.style.background = 
-        "linear-gradient(to right, " 
-        + clr1.value 
-        + ", " 
-        + clr2.value 
-        + ")";
+clr1.addEventListener("input", ColorChanged);
+clr2.addEventListener("input", ColorChanged);
 
+function ColorChanged(){
+    SetBackground(clr1.value, clr2.value);
     h3.textContent = body.style.background + ";";
 }
 
+function SetBackground(c1, c2){
+    body.style.background = 
+        "linear-gradient(to right, " 
+        + c1 
+        + ", " 
+        + c2 
+        + ")";
+}
+
+// function ConverHextoRGB(h){
+//     let hex, r, g, b;
+
+//     hex = h.replace('#','');
+//     r = parseInt(hex.substring(0,2), 16);
+//     g = parseInt(hex.substring(2,4), 16);
+//     b = parseInt(hex.substring(4,6), 16);
+
+//     let rgb = 'rgba(' + r + ', ' + g + ', ' + b + ')';
+//     return rgb;
+// }
